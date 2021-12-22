510bccddbbee8ba019daef24302a99d5cc0d9780
shyamanthawijesiri <shyamanthawijesiri33@gmail.com>
2021-12-22 11:19:08 +0530

    enable cors

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const {mongoose} = require('./db');
 var employeeController = require('./controllers/employeeController');
 
 var app = express();
 app.use(bodyParser.json());
+app.use(cors({origin:'http://localhost:4200'}));
 
 app.listen(3000, ()=> console.log('server is running at port : 3000'));
 
